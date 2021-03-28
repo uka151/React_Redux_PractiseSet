4ce18981fadd69a60e20d572dd0325ec1ed4ab58
uka151 <umeshagrahari87@gmail.com>
2021-03-28 10:00:28 +0530

    Reacrt Redux

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
-const redux=require('redux');
-const createStore=redux.createStore;
-
-
+const redux = require('redux')
+const createStore = redux.createStore
 
 // Action intialize
-const BUY_CAKE ="BUY_CAKE";
+const BUY_CAKE = 'BUY_CAKE'
 
 // Action Type
 function buyCake(){
     return{
-        type:BUY_CAKE,
-        info: "First redux action"
+        type: BUY_CAKE,
+        info: 'First redux action'
     }  
 }
 
 // state intiallize
-
 const initialState = {
-    numberOfCake:10
+    numOfCake:10
 }
 
 // reducer function that take input action + previous state
 
-const reducer=(state=initialState,action)=>{     
-    switch(action.type){
-     case BUY_CAKE: return{
-         ...state,                            // if we want to not update orginal state then we have use spread operator to make of copy of state object
-         numberOfCake:state.numberOfCake-1
+const reducer = (state = initialState, action) => {
+
+    switch(action.type) {
+
+     case BUY_CAKE: return { 
+         ...state,                           // if we want to not update orginal state then we have use spread operator to make of copy of state object
+         numOfCake: state.numOfCake - 1
      }
 
      default: return state;
     }
 }
 
+
+
 //Implement  store & its responsiblity
 
 // First responsibility is hold Application state & take input reducer function
-const store=createStore(reducer);
+const store = createStore(reducer)
 // Second responsibility Allow to acces the state via "getState()" using syntax store.getState()
-console.log("Initial state:"+store.getState());
+console.log("Initial state:",store.getState());
 //Fourth responsibilty is Register lisnter via "subscribe(lisnter)"
-const unsubscribe=store.subscribe(()=>console.log("Update state:"+ store.getState()));     // unsubscribed is fifth step 
+ const unsubscribe=store.subscribe(()=>console.log("Update state:", store.getState()));     // unsubscribed is fifth step 
 
 // Third responsibilty allow state to be updated via dispatch(action);
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 // Fifth & last responsibility is unsubscribed the register lisnter;
+store.dispatch(buyCake());
 unsubscribe();
   
